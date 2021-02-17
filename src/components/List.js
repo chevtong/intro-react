302e4b5247626aad21dcfd55a811e8b2e30fa0a6
chevtong <chevtongwingyan@gmail.com>
2021-02-17 22:56:04 +0100

    Build the completeHandler

--- src/components/List.js
+++ src/components/List.js
@@ -7,27 +7,48 @@ function List({ todos, setTodos }) {
   // filter it by getting the id is not matching
   //then use setTodos to push to the state
   const deleteHandler = (id) => {
-
     let originalArray = [...todos];
 
-    let newArray = originalArray.filter(item => item.id !== id);
+    let newArray = originalArray.filter((item) => item.id !== id);
 
     //console.log(newArray);
 
     setTodos(newArray);
   };
 
+  //get the id from the selected item then pass to the function
+  //grab the todos from props, then use map to loop thru
+  //find the one with the same id and change it to the opposite
+  const completeHandler = (id) => {
+
+    let newArray = [...todos];
+
+    newArray.map((item) => {
+      if(item.id == id){
+        item.complete =! item.complete
+      }
+    })
+   
+    //without setTodos, the UI of checkbox will not be changed
+    //since we set the input checked value to {todo.complete}
+    setTodos(newArray)
+
+  };
+
   return (
     <div className="list">
       <ul>
         {todos.map((todo) => (
-         
           <li key={todo.id}>
-            <input type="checkbox" />
+            <input checked={todo.complete} onChange={() => completeHandler(todo.id)} 
+              type="checkbox" />
+
             {todo.name}
+
             <button
               onClick={() => deleteHandler(todo.id)}
-              className="trash-btn">
+              className="trash-btn"
+            >
               <i className="fas fa-trash"></i>
             </button>
           </li>
