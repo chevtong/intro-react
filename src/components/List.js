fe674fb0aefe89885b8b0a9515b2d0f3166c0d47
chevtong <chevtongwingyan@gmail.com>
2021-02-17 22:03:26 +0100

    Finish deleteHandler

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,36 @@
 import React from "react";
-import Item from "./Item";
 
 function List({ todos, setTodos }) {
 
+  //get the id from the selected item then pass it to the function
+  //create a new array from ...todos (a safer way to change the values of state)
+  // filter it by getting the id is not matching
+  //then use setTodos to push to the state
+  const deleteHandler = (id) => {
 
-  
+    let originalArray = [...todos];
+
+    let newArray = originalArray.filter(item => item.id !== id);
+
+    //console.log(newArray);
+
+    setTodos(newArray);
+  };
+
   return (
     <div className="list">
       <ul>
         {todos.map((todo) => (
-          <Item 
-            key={todo.id}
-            todos={todos}
-            setTodos={setTodos}
-            todo={todo} 
-          />
+         
+          <li key={todo.id}>
+            <input type="checkbox" />
+            {todo.name}
+            <button
+              onClick={() => deleteHandler(todo.id)}
+              className="trash-btn">
+              <i className="fas fa-trash"></i>
+            </button>
+          </li>
         ))}
       </ul>
 
